ee02cf4716987eaeec0bccad735406b2970cd86b
sk1039-hub <sk1039@srmist.edu.in>
2026-02-24 09:22:08 +0530

    [shiva] added Palindrome Check Using String Reverse

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,24 @@
 public class PalindromeCheckerApp {
-    public static void main (String[] args){
-        System.out.println("Welcome to palindrome checker app mangement");
+    public static void main(String[] args) {
+
+                // Original string
+                String original = "level";
+
+                // Variable to store reversed string
+                String reversed = "";
+
+                // Reverse string using for loop
+                for (int i = original.length() - 1; i >= 0; i--) {
+                    reversed = reversed + original.charAt(i);  // String concatenation
+                }
+
+                // Compare original and reversed using equals()
+                if (original.equals(reversed)) {
+                    System.out.println(original + " is a Palindrome.");
+                } else {
+                    System.out.println(original + " is NOT a Palindrome.");
+                }
+            }
+        }
     }
 }
