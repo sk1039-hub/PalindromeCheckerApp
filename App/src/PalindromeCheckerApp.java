e59db8d5686a5e3eda91bf44732e803c4dcbdcc8
sk1039-hub <sk1039@srmist.edu.in>
2026-02-24 09:12:22 +0530

    [shiva] Added UC2: Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,29 @@
 public class PalindromeCheckerApp {
     public static void main (String[] args){
         System.out.println("Welcome to palindrome checker app mangement");
+
+    }
+}
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string (String Literal)
+        String word = "madam";
+
+        // Variable to store reversed string
+        String reversed = "";
+
+        // Reverse the string using loop
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
